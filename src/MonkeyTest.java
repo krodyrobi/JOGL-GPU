d7b3c87c3afcd2902bf7d42ade15a89417dfd377
Krody Robert <robi@presslabs.com>
2014-05-17 12:35:23 +0300

    Pre merge develop

---
+++ src/MonkeyTest.java
@@ -0,0 +1,26 @@
+import javax.media.opengl.GLAutoDrawable;
+
+public class MonkeyTest extends ATestCase {
+
+
+
+    @Override
+    public void init(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    @Override
+    public void dispose(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    @Override
+    public void display(GLAutoDrawable glAutoDrawable) {
+
+    }
+
+    @Override
+    public void reshape(GLAutoDrawable glAutoDrawable, int i, int i2, int i3, int i4) {
+
+    }
+}
